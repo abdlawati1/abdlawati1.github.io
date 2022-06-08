4a8511addca891b8a89a5c2122d5d03bf9fb4706
abdlawati1 <107140515+abdlawati1@users.noreply.github.com>
2022-06-08 22:06:02 +0400

    Update stories.js

--- blog/stories.js
+++ blog/stories.js
@@ -22,7 +22,7 @@
 //	"id": The ID of the story. Come up with a unique ID for each story and never change it (affects the URL). Only use letters and/or numbers.
 //	"date": Date of when the story was added.
 //	"title": Title of the story.
-//	"type": Type of cancer. Available options: prostate, breast, melanoma, testis, blood, cns, head, lung, thyroid, colorecta, liver, kidney, sarcoma, bladder, stomach, uterus.
+//	"type": Type of cancer. Available options: prostate, breast, melanoma, testis, blood, cns, head, lung, thyroid, colorectal, liver, kidney, sarcoma, bladder, stomach, uterus.
 //	"image": File name of the image to use. Must include the file extension (e.g., .jpg, .png). Place all images in the "images" folder.
 //	"abstract": 1 paragraph abstract of the story.
 //	"full": Multi-line text of the full story. Separate each line with 3 dashes (---).
@@ -41,6 +41,21 @@ var cancerStories = [
 		"abstract": "yo wassup people",
 		"full": `
 			yo wassup everyone. POGGERZ
+      --- 
+      POGGERZ
+      ---
+      POGGERZ
+			`,
+	}, 
+	{
+	"id": "test1",
+		"date": "05/06/2000",
+		"title": "مجاهد",
+		"type": "colorectal",
+		"image": "test.jpg",
+		"abstract": "abdlawati1 is a legend",
+		"full": `
+			He is built different
       --- 
       POGGERZ
       ---
