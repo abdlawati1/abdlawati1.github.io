4cb9085ff0fec40945f7beb43c453218110ecfcd
JAlyahmadi <89706055+JAlyahmadi@users.noreply.github.com>
2022-06-13 01:14:49 +0400

    Update stories.js

--- blog/stories.js
+++ blog/stories.js
@@ -22,7 +22,7 @@
 //	"id": The ID of the story. Come up with a unique ID for each story and never change it (affects the URL). Only use letters and/or numbers.
 //	"date": Date of when the story was added.
 //	"title": Title of the story.
-//	"type": Type of cancer. Available options: prostate, breast, melanoma, testis, blood, cns, head, lung, thyroid, colorectal, liver, kidney, sarcoma, bladder, stomach, uterus.
+//	"type": Type of cancer. Available options: prostate, breast, melanoma, testis, blood, cns, head, lung, thyroid, colorectal, liver, kidney, sarcoma, bladder, stomach, uterus, others.
 //	"image": File name of the image to use. Must include the file extension (e.g., .jpg, .png). Place all images in the "images" folder.
 //	"abstract": 1 paragraph abstract of the story.
 //	"full": Multi-line text of the full story. Separate each line with 3 dashes (---).
